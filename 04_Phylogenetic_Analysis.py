13be572f1e0c86d74c8b3c8bcc975f7024fc8c29
nmoreyra <niconm89@gmail.com>
2022-07-30 18:46:27 -0300

    adding 04_Phylogenetic_Analysis.py

--- 04_Phylogenetic_Analysis.py
+++ 04_Phylogenetic_Analysis.py
@@ -3,18 +3,52 @@
 """
 Created on Tue Jul 26 00:37:16 2022
 
-@author: root
+@author: Nicolás Nahuel Moreyra (niconm89@gmail.com)
 """
 #%% Imports
+import argparse
+#import os
 import subprocess
+#from Bio import SeqIO
+from time import time	
 
 #%% Functionts definition
-
-def model_partitions(MATRIXFILE, PARTITIONFILE, PREFIX, BOOTSTRAP, THREADS):
+def model_partitions(MATRIXFILE, PARTITIONFILE, OUTDIR, PREFIX, BOOTSTRAP, THREADS):
 	#iqtree -s supermatrix.aln.faa.phy -p partitions-scheme.txt -m MFP 
 	#--seqtype AA --prefix Drosophila -B 1000 --mem 50 -T 8
-	
+	print("Starting the phylogenetic analysis with IQ-Tree")#using arguments: %s\t%s\t%s\t%n\t%n")
 	IQTree_MFP = "/home/nmoreyra/Soft/miniconda3/envs/spyder/bin/iqtree -s " + MATRIXFILE + " -p " + PARTITIONFILE + " -m MFP --seqtype AA --prefix " + PREFIX + " -B " + str(BOOTSTRAP) + " -T " + str(THREADS)
-	process = subprocess.Popen(IQTree_MFP.split(), stdout=subprocess.PIPE)
-	output, error = process.communicate()
+	print(IQTree_MFP)
+	#process = subprocess.Popen(IQTree_MFP.split(), stdout=subprocess.PIPE)
+	#output, error = process.communicate()
+	subprocess.call([IQTree_MFP], shell=True)	
+	print("Finishing IQ-Tree")
 #end
+
+#%% Menu -> Usage
+def usage():
+	parser = argparse.ArgumentParser(
+		description='''03_create_matrix.py creates a phylogenetic matrix with coordinates by concatenating alignment files in fasta format.''',
+		epilog="""End of the help""")
+
+	parser.add_argument('-m', '--matrix', type=str, required=True, help='Path to the phylogenetic matrix file to run.')
+	parser.add_argument('-p', '--partitions', type=str, required=True, help='Partitions file in nexus format.')
+	parser.add_argument('-o', '--outdir', type=str, required=True, help='Path to save the phylogenetic analysis results. If it does not exists, it will be created.')
+	parser.add_argument('-P', '--prefix', type=str, required=False, default="iqtree", help='Prefix to name the output dataset and results.')
+	parser.add_argument('-b', '--bootstrap', type=int, required=False, default="1000", help='Number of bootstrap replicate to run.')
+	parser.add_argument('-t', '--threads', type=int, required=False, default="16", help='Number of threads to use in IQ-Tree.')
+
+	return parser.parse_args()
+
+#%% Main program
+if __name__ == '__main__':
+	args = usage()
+	start = time()
+	model_partitions(args.matrix, args.partitions, args.outdir, args.prefix, args.bootstrap, args.threads)
+	print("Phylogenetic matrix created...")
+	print(f'Time taken to run: {time() - start} seconds.')
+#
+'''
+iqtree -s ../05_Matrix_Partitions/phylomatrix.phylip -p ../05_Matrix_Partitions/busco_coords.partitions.tsv -m MFP \
+--seqtype AA --prefix Pyraloidea -B 1000 -T 8
+'''
